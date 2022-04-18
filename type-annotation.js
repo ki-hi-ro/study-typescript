db11cee036658fb34b61007b8432f86266607b23
ki-hi-ro <hiroki.hiroki@icloud.com>
2022-04-18 20:59:12 +0900

    過去の内容のコメントアウト

--- type-annotation.js
+++ type-annotation.js
@@ -1,25 +1,25 @@
-var my_name = 'Shibata Hiroki';
-console.log(my_name.toLowerCase());
-var empty_name = "";
-var firstName = 'Hiroki';
-var lastName = 'Shibata';
-var fullName = "".concat(firstName, " ").concat(lastName);
-console.log(my_name);
-console.log(fullName);
-var isOpen = true;
-var disabled = false;
-var list = [1, 2, 3];
-list.push(4);
-console.log(list);
-var sayHello = function (name) {
-    return "Hello, ".concat(name, "!");
-};
-console.log(sayHello('shibata'));
-var person = {
-    name: 'Shibata',
-    age: 28
-};
-console.log(person.name);
+// var my_name = 'Shibata Hiroki';
+// console.log(my_name.toLowerCase());
+// var empty_name = "";
+// var firstName = 'Hiroki';
+// var lastName = 'Shibata';
+// var fullName = "".concat(firstName, " ").concat(lastName);
+// console.log(my_name);
+// console.log(fullName);
+// var isOpen = true;
+// var disabled = false;
+// var list = [1, 2, 3];
+// list.push(4);
+// console.log(list);
+// var sayHello = function (name) {
+//     return "Hello, ".concat(name, "!");
+// };
+// console.log(sayHello('shibata'));
+// var person = {
+//     name: 'Shibata',
+//     age: 28
+// };
+// console.log(person.name);
 // interface Person {
 //   name: string
 //   age: number
@@ -30,15 +30,15 @@ console.log(person.name);
 // const getAge = (person: Person) => {
 //   return person.age
 // }
-var my_name_second = 'Shibata Hiroki';
-my_name_second.toUpperCase();
-// const ageList = [30, 40, 50]
-// ageList[0].toUpperCase()
-var sayHello_second = function (name) {
-    return "Hello, ".concat(name, "!");
-};
-var getFullName = function (person) {
-    return "".concat(person.firstName, " ").concat(person.lastName);
-};
-var largeFullName = console.log(getFullName({ firstName: 'Shibata', lastName: 'Hiroki', age: 28 }).toUpperCase());
-var myName = 'Shibata Hiroki';
+// var my_name_second = 'Shibata Hiroki';
+// my_name_second.toUpperCase();
+const ageList = [30, 40, 50]
+ageList[0].toUpperCase()
+// var sayHello_second = function (name) {
+//     return "Hello, ".concat(name, "!");
+// };
+// var getFullName = function (person) {
+//     return "".concat(person.firstName, " ").concat(person.lastName);
+// };
+// var largeFullName = console.log(getFullName({ firstName: 'Shibata', lastName: 'Hiroki', age: 28 }).toUpperCase());
+// var myName = 'Shibata Hiroki';
