d39870cd72b4c7179e4678b9f9cc9721c64a2f8e
ki-hi-ro <hiroki.hiroki@icloud.com>
2022-04-24 16:43:31 +0900

    モードという概念の導入

--- node-app/src/index.ts
+++ node-app/src/index.ts
@@ -13,6 +13,11 @@ class HitAndBlow {
   private readonly answerSource = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
   private answer: string[] = []
   private tryCount = 0
+  private mode: 'normal' | 'hard'
+
+  constructor(mode: 'normal' | 'hard') {
+    this.mode = mode
+  }
 
   setting() {
     const answerLength = 3
@@ -34,7 +39,7 @@ class HitAndBlow {
       await this.play()
       return
     }
-    
+
     const result = this.check(inputArr)
 
     if (result.hit !== this.answer.length) {
@@ -78,7 +83,7 @@ class HitAndBlow {
 }
 
   ;(async () => {
-    const hitAndBlow = new HitAndBlow()
+    const hitAndBlow = new HitAndBlow('normal')
     hitAndBlow.setting()
     await hitAndBlow.play()
     hitAndBlow.end()
