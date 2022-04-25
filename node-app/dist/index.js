df8331464b214c5e7b63256b16a0e490b2ec05e3
ki-hi-ro <hiroki.hiroki@icloud.com>
2022-04-25 21:49:36 +0900

    ユーザーがモードを選択できるようにした

--- node-app/dist/index.js
+++ node-app/dist/index.js
@@ -55,21 +55,34 @@ var promptInput = function (text) { return __awaiter(void 0, void 0, void 0, fun
     });
 }); };
 var HitAndBlow = /** @class */ (function () {
-    function HitAndBlow(mode) {
+    function HitAndBlow() {
         this.answerSource = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
         this.answer = [];
         this.tryCount = 0;
-        this.mode = mode;
+        this.mode = 'normal';
     }
     HitAndBlow.prototype.setting = function () {
-        var answerLength = this.getAnswerLength();
-        while (this.answer.length < answerLength) {
-            var randNum = Math.floor(Math.random() * this.answerSource.length);
-            var selectedItem = this.answerSource[randNum];
-            if (!this.answer.includes(selectedItem)) {
-                this.answer.push(selectedItem);
-            }
-        }
+        return __awaiter(this, void 0, void 0, function () {
+            var _a, answerLength, randNum, selectedItem;
+            return __generator(this, function (_b) {
+                switch (_b.label) {
+                    case 0:
+                        _a = this;
+                        return [4 /*yield*/, promptInput('モードを入力してください。')];
+                    case 1:
+                        _a.mode = (_b.sent());
+                        answerLength = this.getAnswerLength();
+                        while (this.answer.length < answerLength) {
+                            randNum = Math.floor(Math.random() * this.answerSource.length);
+                            selectedItem = this.answerSource[randNum];
+                            if (!this.answer.includes(selectedItem)) {
+                                this.answer.push(selectedItem);
+                            }
+                        }
+                        return [2 /*return*/];
+                }
+            });
+        });
     };
     HitAndBlow.prototype.play = function () {
         return __awaiter(this, void 0, void 0, function () {
@@ -138,6 +151,9 @@ var HitAndBlow = /** @class */ (function () {
                 return 3;
             case 'hard':
                 return 4;
+            default:
+                var neverValue = this.mode;
+                throw new Error(neverValue + " \u306F\u7121\u52B9\u306A\u30E2\u30FC\u30C9\u3067\u3059\u3002");
         }
     };
     return HitAndBlow;
@@ -148,10 +164,12 @@ var HitAndBlow = /** @class */ (function () {
     return __generator(this, function (_a) {
         switch (_a.label) {
             case 0:
-                hitAndBlow = new HitAndBlow('normal');
-                hitAndBlow.setting();
-                return [4 /*yield*/, hitAndBlow.play()];
+                hitAndBlow = new HitAndBlow();
+                return [4 /*yield*/, hitAndBlow.setting()];
             case 1:
+                _a.sent();
+                return [4 /*yield*/, hitAndBlow.play()];
+            case 2:
                 _a.sent();
                 hitAndBlow.end();
                 return [2 /*return*/];
--- node-app/src/index.ts
+++ node-app/src/index.ts
@@ -15,13 +15,10 @@ class HitAndBlow {
   private readonly answerSource = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
   private answer: string[] = []
   private tryCount = 0
-  private mode: Mode
+  private mode: Mode = 'normal'
 
-  constructor(mode: Mode) {
-    this.mode = mode
-  }
-
-  setting() {
+  async setting() {
+    this.mode = await promptInput('モードを入力してください。') as Mode
     const answerLength = this.getAnswerLength()
 
     while (this.answer.length < answerLength) {
@@ -98,8 +95,8 @@ class HitAndBlow {
 }
 
   ;(async () => {
-    const hitAndBlow = new HitAndBlow('normal')
-    hitAndBlow.setting()
+    const hitAndBlow = new HitAndBlow()
+    await hitAndBlow.setting()
     await hitAndBlow.play()
     hitAndBlow.end()
   })()
