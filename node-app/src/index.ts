1dad7e16d4151e38423031d8c1f83890a8ccd32e
ki-hi-ro <hiroki.hiroki@icloud.com>
2022-05-01 18:40:32 +0900

    ジャンケンクラスの作成

--- node-app/src/index.ts
+++ node-app/src/index.ts
@@ -2,18 +2,18 @@ const printLine = (text: string, breakLine: boolean = true) => {
   process.stdout.write(text + (breakLine ? '\n' : ''))
 }
 
-const promptInput = async (text: string) => {
-  printLine(`\n${text}\n>`, false)
-  return readLine()
-}
-
 const readLine = async () => {
   const input: string = await new Promise((resolve) =>
                           process.stdin.once('data', (data) => resolve(data.toString())))
   return input.trim()
 }
 
-const promptSelect = async <T extends string>(text: string, values: readonly string[]): Promise<T> => {
+const promptInput = async (text: string) => {
+  printLine(`\n${text}\n> `, false)
+  return readLine()
+}
+
+const promptSelect = async <T extends string>(text: string, values: readonly T[]): Promise<T> => {
   printLine(`\n${text}`)
   values.forEach((value) => {
     printLine(`- ${value}`)
@@ -27,9 +27,6 @@ const promptSelect = async <T extends string>(text: string, values: readonly str
     return promptSelect<T>(text, values)
   }
 }
-
-const modes = ['normal', 'hard'] as const
-type Mode = typeof modes[number]
 
 const nextActions = ['play again', 'exit'] as const
 type NextAction = typeof nextActions[number]
@@ -64,6 +61,9 @@ class GameProcedure {
     process.exit()
   }
 }
+
+const modes = ['normal', 'hard'] as const
+type Mode = typeof modes[number]
 
 class HitAndBlow {
   private readonly answerSource = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -105,6 +105,16 @@ class HitAndBlow {
     }
   }
 
+  end() {
+    printLine(`正解です！\n試行回数: ${this.tryCount}回\n`)
+    this.reset()
+  }
+
+  private reset() {
+    this.answer = []
+    this.tryCount = 0
+  }
+
   private check(input: string[]) {
     let hitCount = 0
     let blowCount = 0
@@ -123,21 +133,11 @@ class HitAndBlow {
     }
   }
 
-  end() {
-    printLine(`正解です！\n試行回数: ${this.tryCount}回\n`)
-    this.reset()
-  }
-
-  private reset() {
-    this.answer = []
-    this.tryCount = 0
-  }
-
   private validate(inputArr: string[]) {
     const isLengthValid = inputArr.length === this.answer.length
-    const isAllAnswerSourseOption = inputArr.every((val) => this.answerSource.includes(val))
+    const isAllAnswerSourceOption = inputArr.every((val) => this.answerSource.includes(val))
     const isAllDifferentValues = inputArr.every((val, i) => inputArr.indexOf(val) === i)
-    return isLengthValid && isAllAnswerSourseOption && isAllDifferentValues
+    return isLengthValid && isAllAnswerSourceOption && isAllDifferentValues
   }
 
   private getAnswerLength() {
@@ -149,6 +149,87 @@ class HitAndBlow {
       default:
         const neverValue: never = this.mode
         throw new Error(`${neverValue} は無効なモードです。`)
+    }
+  }
+}
+
+const jankenOptions = ['rock', 'paper', 'scissors'] as const
+type JankenOption = typeof jankenOptions[number]
+
+class Janken {
+  private rounds = 0
+  private currentRound = 1
+  private result = {
+    win: 0,
+    lose: 0,
+    draw: 0,
+  }
+
+  async setting() {
+    const rounds = Number(await promptInput('何本勝負にしますか？'))
+    if (Number.isInteger(rounds) && 0 < rounds) {
+      this.rounds = rounds
+    } else {
+      await this.setting()
+    }
+  }
+
+  async play() {
+    const userSelected = await promptSelect(`【${this.currentRound}回戦】選択肢を入力してください。`, jankenOptions)
+    const randomSelected = jankenOptions[Math.floor(Math.random() * 3)]
+    const result = Janken.judge(userSelected, randomSelected)
+    let resultText: string
+
+    switch (result) {
+      case 'win':
+        this.result.win += 1
+        resultText = '勝ち'
+        break
+      case 'lose':
+        this.result.lose += 1
+        resultText = '負け'
+        break
+      case 'draw':
+        this.result.draw += 1
+        resultText = 'あいこ'
+        break
+    }
+    printLine(`---\nあなた: ${userSelected}\n相手${randomSelected}\n${resultText}\n---`)
+
+    if (this.currentRound < this.rounds) {
+      this.currentRound += 1
+      await this.play()
+    }
+  }
+
+  end() {
+    printLine(`\n${this.result.win}勝${this.result.lose}敗${this.result.draw}引き分けでした。`)
+    this.reset()
+  }
+
+  private reset() {
+    this.rounds = 0
+    this.currentRound = 1
+    this.result = {
+      win: 0,
+      lose: 0,
+      draw: 0,
+    }
+  }
+
+  static judge(userSelected: JankenOption, randomSelected: JankenOption) {
+    if (userSelected === 'rock') {
+      if (randomSelected === 'rock') return 'draw'
+      if (randomSelected === 'paper') return 'lose'
+      return 'win'
+    } else if (userSelected === 'paper') {
+      if (randomSelected === 'rock') return 'win'
+      if (randomSelected === 'paper') return 'draw'
+      return 'lose'
+    } else {
+      if (randomSelected === 'rock') return 'lose'
+      if (randomSelected === 'paper') return 'win'
+      return 'draw'
     }
   }
 }
